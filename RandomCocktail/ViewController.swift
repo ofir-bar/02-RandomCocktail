0465f1379ed834d6c2d65de49c9df7bf9b069209
Ofir Bar <ofirbar5@gmail.com>
2019-06-21 13:47:28 +0300

    first commit

---
+++ RandomCocktail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomCocktail
+//
+//  Created by ofir bar on 21/06/2019.
+//  Copyright © 2019 Ofir Bar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
